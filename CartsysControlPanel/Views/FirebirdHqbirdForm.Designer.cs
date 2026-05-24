81b46db63fcaabe624417d9358cbe98cb59daac3
Lucas Pieroni <lucaspieroni819918@outlook.com>
2026-05-24 18:30:20 -0300

    feat: implementa formulário responsável pelas instalações e configurações do firebird e hqbird

---
+++ CartsysControlPanel/Views/FirebirdHqbirdForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace CartsysControlPanel.Views
+{
+    partial class FirebirdHqbirdForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnDirectDownload = new Button();
+            btnHqbirdPage = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            btnFirebirdInstall = new Button();
+            label3 = new Label();
+            btnFirebirdConfig = new Button();
+            btnDbcrypt = new Button();
+            btnBackupFolder = new Button();
+            btnUdr = new Button();
+            btnAllConfig = new Button();
+            SuspendLayout();
+            // 
+            // btnDirectDownload
+            // 
+            btnDirectDownload.BackColor = Color.FromArgb(100, 116, 139);
+            btnDirectDownload.FlatAppearance.BorderSize = 0;
+            btnDirectDownload.FlatStyle = FlatStyle.Flat;
+            btnDirectDownload.ForeColor = Color.FromArgb(226, 232, 240);
+            btnDirectDownload.Location = new Point(39, 83);
+            btnDirectDownload.Name = "btnDirectDownload";
+            btnDirectDownload.Size = new Size(119, 38);
+            btnDirectDownload.TabIndex = 3;
+            btnDirectDownload.Text = "Download Direto";
+            btnDirectDownload.UseVisualStyleBackColor = false;
+            btnDirectDownload.Click += btnDirectDownload_Click;
+            // 
+            // btnHqbirdPage
+            // 
+            btnHqbirdPage.BackColor = Color.FromArgb(100, 116, 139);
+            btnHqbirdPage.FlatAppearance.BorderSize = 0;
+            btnHqbirdPage.FlatStyle = FlatStyle.Flat;
+            btnHqbirdPage.ForeColor = Color.FromArgb(226, 232, 240);
+            btnHqbirdPage.Location = new Point(203, 83);
+            btnHqbirdPage.Name = "btnHqbirdPage";
+            btnHqbirdPage.Size = new Size(119, 38);
+            btnHqbirdPage.TabIndex = 4;
+            btnHqbirdPage.Text = "Página Oficial HQBird";
+            btnHqbirdPage.UseVisualStyleBackColor = false;
+            btnHqbirdPage.Click += btnHqbirdPage_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11F);
+            label1.Location = new Point(39, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(153, 20);
+            label1.TabIndex = 5;
+            label1.Text = "DOWNLOAD HQBIRD";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F);
+            label2.Location = new Point(39, 158);
+            label2.Name = "label2";
+            label2.Size = new Size(163, 20);
+            label2.TabIndex = 6;
+            label2.Text = "INSTALAR FIREBIRD 3.0";
+            // 
+            // btnFirebirdInstall
+            // 
+            btnFirebirdInstall.BackColor = Color.FromArgb(100, 116, 139);
+            btnFirebirdInstall.FlatAppearance.BorderSize = 0;
+            btnFirebirdInstall.FlatStyle = FlatStyle.Flat;
+            btnFirebirdInstall.ForeColor = Color.FromArgb(226, 232, 240);
+            btnFirebirdInstall.Location = new Point(39, 200);
+            btnFirebirdInstall.Name = "btnFirebirdInstall";
+            btnFirebirdInstall.Size = new Size(119, 38);
+            btnFirebirdInstall.TabIndex = 7;
+            btnFirebirdInstall.Text = "Instalar Firebird";
+            btnFirebirdInstall.UseVisualStyleBackColor = false;
+            btnFirebirdInstall.Click += btnFirebirdInstall_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F);
+            label3.Location = new Point(39, 270);
+            label3.Name = "label3";
+            label3.Size = new Size(230, 20);
+            label3.TabIndex = 8;
+            label3.Text = "CONFIGURAÇÕES/CRIPTOGRAFIA";
+            // 
+            // btnFirebirdConfig
+            // 
+            btnFirebirdConfig.BackColor = Color.FromArgb(100, 116, 139);
+            btnFirebirdConfig.FlatAppearance.BorderSize = 0;
+            btnFirebirdConfig.FlatStyle = FlatStyle.Flat;
+            btnFirebirdConfig.ForeColor = Color.FromArgb(226, 232, 240);
+            btnFirebirdConfig.Location = new Point(39, 308);
+            btnFirebirdConfig.Name = "btnFirebirdConfig";
+            btnFirebirdConfig.Size = new Size(119, 38);
+            btnFirebirdConfig.TabIndex = 9;
+            btnFirebirdConfig.Text = "Aplicar Firebird.conf";
+            btnFirebirdConfig.UseVisualStyleBackColor = false;
+            btnFirebirdConfig.Click += btnFirebirdConfig_Click;
+            // 
+            // btnDbcrypt
+            // 
+            btnDbcrypt.BackColor = Color.FromArgb(100, 116, 139);
+            btnDbcrypt.FlatAppearance.BorderSize = 0;
+            btnDbcrypt.FlatStyle = FlatStyle.Flat;
+            btnDbcrypt.ForeColor = Color.FromArgb(226, 232, 240);
+            btnDbcrypt.Location = new Point(203, 308);
+            btnDbcrypt.Name = "btnDbcrypt";
+            btnDbcrypt.Size = new Size(119, 38);
+            btnDbcrypt.TabIndex = 10;
+            btnDbcrypt.Text = "Aplicar DbCrypt.conf";
+            btnDbcrypt.UseVisualStyleBackColor = false;
+            btnDbcrypt.Click += btnDbcrypt_Click;
+            // 
+            // btnBackupFolder
+            // 
+            btnBackupFolder.BackColor = Color.FromArgb(100, 116, 139);
+            btnBackupFolder.FlatAppearance.BorderSize = 0;
+            btnBackupFolder.FlatStyle = FlatStyle.Flat;
+            btnBackupFolder.ForeColor = Color.FromArgb(226, 232, 240);
+            btnBackupFolder.Location = new Point(39, 375);
+            btnBackupFolder.Name = "btnBackupFolder";
+            btnBackupFolder.Size = new Size(119, 38);
+            btnBackupFolder.TabIndex = 11;
+            btnBackupFolder.Text = "Configurar Pasta de Backup";
+            btnBackupFolder.UseVisualStyleBackColor = false;
+            btnBackupFolder.Click += btnBackupFolder_Click;
+            // 
+            // btnUdr
+            // 
+            btnUdr.BackColor = Color.FromArgb(100, 116, 139);
+            btnUdr.FlatAppearance.BorderSize = 0;
+            btnUdr.FlatStyle = FlatStyle.Flat;
+            btnUdr.ForeColor = Color.FromArgb(226, 232, 240);
+            btnUdr.Location = new Point(203, 375);
+            btnUdr.Name = "btnUdr";
+            btnUdr.Size = new Size(119, 38);
+            btnUdr.TabIndex = 12;
+            btnUdr.Text = "Configurar UDR_SC.dll";
+            btnUdr.UseVisualStyleBackColor = false;
+            btnUdr.Click += btnUdr_Click;
+            // 
+            // btnAllConfig
+            // 
+            btnAllConfig.BackColor = Color.FromArgb(100, 116, 139);
+            btnAllConfig.FlatAppearance.BorderSize = 0;
+            btnAllConfig.FlatStyle = FlatStyle.Flat;
+            btnAllConfig.ForeColor = Color.FromArgb(226, 232, 240);
+            btnAllConfig.Location = new Point(39, 453);
+            btnAllConfig.Name = "btnAllConfig";
+            btnAllConfig.Size = new Size(283, 38);
+            btnAllConfig.TabIndex = 13;
+            btnAllConfig.Text = "Executar configuração completa";
+            btnAllConfig.UseVisualStyleBackColor = false;
+            btnAllConfig.Click += btnAllConfig_Click;
+            // 
+            // FirebirdHqbirdForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(17, 24, 39);
+            ClientSize = new Size(1264, 681);
+            Controls.Add(btnAllConfig);
+            Controls.Add(btnUdr);
+            Controls.Add(btnBackupFolder);
+            Controls.Add(btnDbcrypt);
+            Controls.Add(btnFirebirdConfig);
+            Controls.Add(label3);
+            Controls.Add(btnFirebirdInstall);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnHqbirdPage);
+            Controls.Add(btnDirectDownload);
+            ForeColor = Color.FromArgb(226, 232, 240);
+            Name = "FirebirdHqbirdForm";
+            Text = "FirebirdHqbirdForm";
+            Load += FirebirdHqbirdForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnFirebirdInstall;
+        private Button btnDirectDownload;
+        private Button btnHqbirdPage;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnFirebirdConfig;
+        private Button btnDbcrypt;
+        private Button btnBackupFolder;
+        private Button btnUdr;
+        private Button btnAllConfig;
+    }
+}
---
+++ CartsysControlPanel/Views/FirebirdHqbirdForm.cs
@@ -0,0 +1,207 @@
+﻿using CartsysControlPanel.Domain;
+using CartsysControlPanel.Infrastructure.Network;
+using CartsysControlPanel.Logging;
+using System.ComponentModel;
+
+namespace CartsysControlPanel.Views
+{
+    public partial class FirebirdHqbirdForm : Form
+    {
+        public FirebirdHqbirdForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnDirectDownload_Click(object sender, EventArgs e)
+        {
+            LoggingFile.Info("Iniciando redirecionamento para o link de download direto do Hqbird.");
+            btnDirectDownload.Enabled = false;
+            bool success = await NetworkHandler.RedirectToHqbird(isDirectDownload: true);
+            if (!success)
+            {
+                LoggingFile.Info("Falha ao redirecionar para o link de download direto do Hqbird.");
+                MessageBox.Show("Falha ao abrir o link de download direto do Hqbird.");
+            }
+            else LoggingFile.Info("Redirecionamento para o link de download direto do Hqbird realizado com sucesso.");
+            btnDirectDownload.Enabled = true;
+
+        }
+
+        private async void btnHqbirdPage_Click(object sender, EventArgs e)
+        {
+            LoggingFile.Info("Iniciando redirecionamento para o link da pagína de download do Hqbird.");
+            btnHqbirdPage.Enabled = false;
+            bool success = await NetworkHandler.RedirectToHqbird(isDirectDownload: false);
+            if (!success)
+            {
+                LoggingFile.Info("Falha ao redirecionar para o link da pagína de download do Hqbird.");
+                MessageBox.Show("Falha ao abrir o link da página de download do Hqbird.");
+            }
+            else LoggingFile.Info("Redirecionamento para o link da página de download do Hqbird realizado com sucesso.");
+
+            btnHqbirdPage.Enabled = true;
+        }
+
+        private async void btnFirebirdInstall_Click(object sender, EventArgs e)
+        {
+            LoggingFile.Info("Iniciando instalação do Firebird.");
+            btnFirebirdInstall.Enabled = false;
+            btnFirebirdInstall.Text = "Instalando...";
+            await Task.Run(() => DependencyManager.InstallFirebird());
+            btnFirebirdInstall.Enabled = true;
+            btnFirebirdInstall.Text = "Instalar Firebird";
+        }
+
+        private async void btnFirebirdConfig_Click(object sender, EventArgs e)
+        {
+            LoggingFile.Info("Iniciando configuração do 'firebird.conf'.");
+            btnFirebirdConfig.Enabled = false;
+            btnFirebirdConfig.Text = "Configurando...";
+            await Task.Run((() =>
+            {
+                try
+                {
+                    DependencyManager.SetFirebirdConfig();
+                    LoggingFile.Info("Configuração do 'firebird.conf' concluída com sucesso.");
+                    Invoke(() =>
+                   MessageBox.Show("Configuração do 'firebird.conf' concluída com sucesso."));
+                }
+                catch (IOException iEx)
+                {
+                    Invoke(() =>
+                   MessageBox.Show($"Falha de I/O ao configurar 'firebird.conf'. Detalhes: {iEx.Message}"));
+                }
+                catch (Exception ex)
+                {
+                    Invoke(() =>
+                   MessageBox.Show($"Erro ao configurar 'firebird.conf': {ex.Message}"));
+                }
+            }));
+            btnFirebirdConfig.Enabled = true;
+            btnFirebirdConfig.Text = "Aplicar Firebird.conf";
+        }
+
+        private async void btnDbcrypt_Click(object sender, EventArgs e)
+        {
+            LoggingFile.Info("Iniciando configuração do 'DbCrypt.conf'.");
+            btnDbcrypt.Enabled = false;
+            btnDbcrypt.Text = "Configurando...";
+            await Task.Run((() =>
+            {
+                try
+                {
+                    DependencyManager.SetDbCrypt();
+                    LoggingFile.Info("Configuração do 'DbCrypt.conf' concluída com sucesso.");
+                    Invoke(() =>
+                   MessageBox.Show("Configuração do DbCrypt concluída com sucesso."));
+                }
+                catch (IOException iEx)
+                {
+                    Invoke(() =>
+                   MessageBox.Show($"Falha de I/O ao configurar 'DbCrypt.conf'. Detalhes: {iEx.Message}"));
+                }
+                catch (Exception ex)
+                {
+                    Invoke(() =>
+                   MessageBox.Show($"Erro ao configurar 'DbCrypt.conf': {ex.Message}"));
+                }
+            }));
+            btnDbcrypt.Enabled = true;
+            btnDbcrypt.Text = "Aplicar DbCrypt.conf";
+
+        }
+
+        private async void btnBackupFolder_Click(object sender, EventArgs e)
+        {
+            LoggingFile.Info("Iniciando configuração da pasta de backup.");
+            btnBackupFolder.Enabled = false;
+            btnBackupFolder.Text = "Configurando...";
+            await Task.Run((() =>
+            {
+                try
+                {
+                    DependencyManager.SetBackupFolder();
+                    LoggingFile.Info("Configuração da pasta de backup concluída com sucesso.");
+                    Invoke(() =>
+                   MessageBox.Show("Configuração da pasta de backup concluída com sucesso."));
+                }
+                catch (IOException iEx)
+                {
+                    Invoke(() =>
+                   MessageBox.Show($"Falha de I/O ao configurar a pasta de backup. Detalhes: {iEx.Message}"));
+                }
+                catch (Exception ex)
+                {
+                    Invoke(() =>
+                   MessageBox.Show($"Erro ao configurar a pasta de backup: {ex.Message}"));
+                }
+            }));
+            btnBackupFolder.Enabled = true;
+            btnBackupFolder.Text = "Configurar Pasta de Backup";
+        }
+
+        private async void btnUdr_Click(object sender, EventArgs e)
+        {
+            LoggingFile.Info("Iniciando configuração do UDR.");
+            btnUdr.Enabled = false;
+            btnUdr.Text = "Configurando...";
+            await Task.Run((() =>
+            {
+                try
+                {
+                    DependencyManager.SetUdrDll();
+                    LoggingFile.Info("Configuração do UDR concluída com sucesso.");
+                    Invoke(() =>
+                   MessageBox.Show("Configuração do UDR concluída com sucesso."));  
+                }
+                catch (IOException iEx)
+                {
+                    Invoke(() =>
+                   MessageBox.Show($"Falha de I/O ao configurar o UDR. Detalhes: {iEx.Message}"));
+                }
+                catch (Exception ex)
+                {
+                    Invoke(() =>
+                   MessageBox.Show($"Erro ao configurar o UDR: {ex.Message}"));
+                }
+            }));
+            btnUdr.Enabled = true;
+            btnUdr.Text = "Configurar UDR_SC.dll";
+        }
+
+        private async void btnAllConfig_Click(object sender, EventArgs e)
+        {
+            LoggingFile.Info("Iniciando configuração de todas as dependências.");
+            btnAllConfig.Enabled = false;
+            btnAllConfig.Text = "Configurando...";
+            await Task.Run((() =>
+            {
+                try
+                {
+                    DependencyManager.SetFirebirdConfig();
+                    DependencyManager.SetDbCrypt();
+                    DependencyManager.SetBackupFolder();
+                    DependencyManager.SetUdrDll();
+                    LoggingFile.Info("Configuração de todas as dependências concluída com sucesso.");
+                    Invoke(() =>
+                   MessageBox.Show("Configuração de todas as dependências concluída com sucesso."));
+                }
+                catch (Exception ex)
+                {
+                    LoggingFile.Error("Configuração completa interrompida.", ex);
+                    Invoke(() => MessageBox.Show(
+                        $"Ocorreu um erro durante a configuração: {ex.Message}\n\nVerifique o log para mais detalhes.",
+                        "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error));
+                }
+            }));
+            btnAllConfig.Enabled = true;
+            btnAllConfig.Text = "Aplicar Todas as Configurações";
+        }
+
+        private void FirebirdHqbirdForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
